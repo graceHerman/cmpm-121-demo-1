c6c2f3f9d84ebca8f611e6f9713a4b572db5cd1c
graceHerman <151581400+graceHerman@users.noreply.github.com>
2024-10-05 11:36:05 -0700

    Step 2 Completed

--- src/main.ts
+++ src/main.ts
@@ -9,13 +9,38 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+// --------------------------------------------------------
+// My edits
+
+// Make a counter
+// let counter: number = 0; didn't work for some reason
+let counter: number = 0;
+
+// Create a div element to display the current counter value
+const counterDisplay = document.createElement("div");
+counterDisplay.innerHTML = counter + " levels completed"; // initial message
+
 // Create the button
-const button = document.createElement('button');
+const button = document.createElement("button");
+
+// Make everything on the webpage centered
+document.body.style.display = "grid";
+// Center items vertically and horizontally
+document.body.style.placeItems = "center";
+// Full height of the viewport
+document.body.style.height = "100vh";
 
 // Make button an emoji, let's use the game controller emoji
-button.innerHTML = '🎮 Play Now';
+button.innerHTML = "🎮 Play Now";
+
+// Add event listener for button
+button.addEventListener("click", () => {
+  counter++;
+  counterDisplay.innerHTML = counter + " levels completed"; // updated message
+});
 
 // Append the button to the body of the document
 document.body.appendChild(button);
+document.body.appendChild(counterDisplay);
 
 // file path: '/Users/gracelilanhermangmail.com/Desktop/Fall 2024/121/cmpm-121-demo-1'
