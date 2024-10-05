d98b63930918608d8288f232a87c63d2ee6636a2
graceHerman <151581400+graceHerman@users.noreply.github.com>
2024-10-05 10:31:55 -0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My amazing game";
+const gameName = "My new amazing game 2.0";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+// // '/Users/gracelilanhermangmail.com/Desktop/Fall 2024/121/cmpm-121-demo-1'
