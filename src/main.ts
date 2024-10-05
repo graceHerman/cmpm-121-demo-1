81ba65c99d446ba4a9896f54cf2f8429fa380b4f
graceHerman <151581400+graceHerman@users.noreply.github.com>
2024-10-05 11:09:36 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -9,4 +9,13 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-// // '/Users/gracelilanhermangmail.com/Desktop/Fall 2024/121/cmpm-121-demo-1'
+// Create the button
+const button = document.createElement('button');
+
+// Make button an emoji, let's use the game controller emoji
+button.innerHTML = '🎮 Play Now';
+
+// Append the button to the body of the document
+document.body.appendChild(button);
+
+// file path: '/Users/gracelilanhermangmail.com/Desktop/Fall 2024/121/cmpm-121-demo-1'
